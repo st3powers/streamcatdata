7f8c5f594b8e8c12ec823d9c65f65307e1e98dc6
stephen powers <st3powers@gmail.com>
2022-04-03 00:00:04 -0500

    add regions 17, 18

--- bigJoinStreamcat_wrapper2022Apr2.R
+++ bigJoinStreamcat_wrapper2022Apr2.R
@@ -39,7 +39,8 @@ region<-"12"
 #regions<-c("10L","10U")
 #regions<-c("11","12")
 #regions<-c("13","14")
-regions<-c("15","16")
+#regions<-c("15","16")
+regions<-c("17","18")
 
 
 for(j in 1:length(regions)){
